a9bba8e91bc952135accf8452e08df0daa447124
kocurc <kordian.o@wp.pl>
2019-06-15 18:19:01 +0200

    New finished exercises.

---
+++ SkryptyFunkcjeModuly/FilesOlderThan.ps1
@@ -0,0 +1,9 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$FolderPath='C:\Users\xxx\Desktop\Test',
+    [Parameter(Mandatory=$True)]
+    [DateTime]$LastWriteDate=(Get-Date).AddYears(-1)
+)
+
+Get-ChildItem $FolderPath | Where-Object -Property LastWriteTime -GT $LastWriteDate
---
+++ SkryptyFunkcjeModuly/utilities.psm1
@@ -0,0 +1,11 @@
+﻿function Get-FilesOlderThan {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [string]$FolderPath='C:\Users\xxx\Desktop\Test',
+        [Parameter(Mandatory=$True)]
+        [DateTime]$LastWriteDate=(Get-Date).AddYears(-1)
+    )
+
+    Get-ChildItem $FolderPath | Where-Object -Property LastWriteTime -GT $LastWriteDate
+}
